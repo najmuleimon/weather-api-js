02e6708ec5a7f51df8db9da9adae7976ffdff49d
najmuleimon <najmul.eimon@gmail.com>
2022-03-12 21:11:51 +0600

    update code and design

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+
+// search weather on button click
 const searchWeather = () => {
     const city = document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
@@ -8,13 +10,28 @@ const searchWeather = () => {
         .then(data => displayTemperature(data));
 }
 
+// press keyboard enter key to search
+document.getElementById('city-name').addEventListener('keypress', function(event){
+    if(event.key === 'Enter'){
+        document.getElementById('search-weather').click()
+    }
+})
+
+// set inner text
 const setInnerText = (id, text) => {
     document.getElementById(id).innerText = text;
 }
 
+// display temperature
 const displayTemperature = temperature => {
     setInnerText('city', temperature.name);
     setInnerText('temperature', temperature.main.temp);
+    if(temperature.main.temp > 30){
+        document.getElementById('temperature').style.color = '#f74949'
+    }
+    else{
+        document.getElementById('temperature').style.color = '#5e89ff'
+    }
     setInnerText('condition', temperature.weather[0].main);
     setInnerText('min-temp', temperature.main.temp_min);
     setInnerText('max-temp', temperature.main.temp_max);
